bef8c080a639b74c5a27a0585333fabb1051009f
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-09-09 23:49:34 +0530

    started a project using redux,built footer,initiated header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <div className='grid text-white grid-cols-4 bg-violet-600 p-7 w-full fixed bottom-0'>
+            <div>
+                <p className='font-bold'><i className="fa-solid fa-truck me-2"></i>Daily cart</p>
+                <p className='text-sm'>Designed and built with all the love in the world by the luminar team with help of our contributors </p>
+                <p className='text-sm'>code licensed Luminar,docs CC BY 3.0</p>
+                <p className='text-sm'>Currently v5.3.2.</p>
+            </div>
+            <div><p className='font-bold'>Links</p>
+                <p className='text-sm'><Link>Landing Page</Link></p>
+                <p className='text-sm'><Link>Home Page</Link></p>
+                <p className='text-sm'><Link>Watch History Page</Link></p>
+            </div>
+            <div><p className='font-bold'> Guides</p>
+                <p className='text-sm'>React</p>
+                <p className='text-sm'>React Bootstrap</p>
+                <p className='text-sm'>React Router</p>
+            </div>
+            <div>
+                <p>Contact Us</p>
+                <div>
+                    
+                    <div className="mt-2 flex ">
+                        <div className="me-2 flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-600">
+                            
+                            <input id="price" type="text" name="price" placeholder="Enter your email here" className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 me-1" />
+                            
+                        </div>
+                        <div className="">🡢</div>
+                    </div>
+                </div>
+                <div className='mt-3'>
+                    <i className="fa-brands fa-twitter me-4"></i>
+                    <i className="fa-brands fa-instagram me-4"></i>
+                    <i className="fa-brands fa-facebook me-4"></i>
+                    <i className="fa-brands fa-linkedin me-4"></i>
+                    <i className="fa-brands fa-github me-4"></i>
+                    <i className="fa-brands fa-whatsapp"></i>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <nav className='flex justify-between p-5 w-full bg-violet-600 text-white fixed font-bold text-2xl'>
+        <Link to='/'><i className="fa-solid fa-truck"></i> <span>Daily Cart</span></Link>
+    <ul className='flex'>
+    <li className='px-5'><Link to='/wishlist'><i className="fa-solid fa-heart text-red-600 me-1"></i><span>Wishlist</span><span className='p-1 bg-black  rounded-full ms-1 '>10</span></Link></li>
+    <li className='px-5'><Link to='/cart'><i className="fa-solid fa-cart-plus text-green-500 me-1"></i><span>Cart</span><span className='p-1 bg-black  rounded-full ms-1'>10</span></Link></li>
+    </ul>
+    </nav>
+  )
+}
+
+export default Header
