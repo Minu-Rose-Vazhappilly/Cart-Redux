3941e7f1ef59b64e0d621597286d36f46a2f1f6e
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-09-11 11:27:06 +0530

    applied redux,fetched details from dummystore to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAllProducts } from '../redux/slices/productSlice'
 
 function Home() {
+  const dispatch = useDispatch()
+  const {loading,error,allProducts} = useSelector((state)=>state.productReducer)
+  console.log(loading,error,allProducts);
+  
+
+  useEffect(()=>{
+    dispatch(fetchAllProducts())
+  },[])
   return (
     <>
     <Header/>
     <div style={{paddingTop:'100px'}} className='mx-5'>
+      {
+        loading && (
+          <p>Loading...</p>
+        )
+      }
         
         <div className="grid grid-cols-4 gap-4 ">
-            <div className="rounded border p-2 shadow">
+           {
+            allProducts?.length>0 ?(
+               allProducts?.map((products)=>(
+                <div className="rounded border p-2 shadow">
                 {/*image */}
-                <img height={'200px'} src="https://1.bp.blogspot.com/-PxDukBAYVgI/UrkAGMwpPdI/AAAAAAAAAbo/Md-9v9Q8UCM/s1600/makeup+forever.jpg" alt="" />
+                <img height={'200px'} src={products.thumbnail} alt="" />
                 {/*title */}
                 <div className='text-center'>
-                    <h3 className='font-bold text-xl '>title</h3>
+                    <h3 className='font-bold text-xl '>{products.title}</h3>
                     {/*link */}
-                    <Link to={'/id/view'} className='bg-violet-900 p-1 rounded text-white mt-3 inline-block'>View more...</Link>
+                    <Link to={`/${products.id}/view`} className='bg-violet-900 p-1 rounded text-white mt-3 inline-block'>View more...</Link>
                 </div>
             </div>
+               ))
+            ):(
+              <p>No Products Found</p>
+            )
+           }
         </div>
     </div>
     </>
